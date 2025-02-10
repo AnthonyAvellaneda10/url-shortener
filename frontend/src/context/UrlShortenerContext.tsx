810d512a9d380ff3f1017125997486435308b8c2
Anthony <anthony_cristhian7@hotmail.com>
2025-02-10 10:36:10 -0500

    fix: update UrlShortenerContext to match new backend changes

--- frontend/src/context/UrlShortenerContext.tsx
+++ frontend/src/context/UrlShortenerContext.tsx
@@ -2,6 +2,8 @@ import { createContext, useContext, useEffect, useState, ReactNode } from "react
 import axios from "axios";
 import toast from "react-hot-toast";
 
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
 interface Link {
   id: string;
   url: string;
@@ -25,13 +27,18 @@ const UrlShortenerContext = createContext<UrlShortenerContextProps | undefined>(
 export const UrlShortenerProvider = ({ children }: { children: ReactNode }) => {
   const [urls, setUrls] = useState<Link[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  // const [isInitialized, setIsInitialized] = useState(false); // Nuevo estado
 
   const fetchUrls = async () => {
+    setIsLoading(true); // ← Activamos el estado de carga
     try {
-      const response = await axios.get("/shorten/list");
+      const response = await axios.get(`${BACKEND_URL}/shorten/list`);
       setUrls(response.data);
     } catch (error) {
       console.error("Error fetching URLs", error);
+    } finally {
+      setTimeout(() => setIsLoading(false), 500);
+      // setIsInitialized(true); // Marcar como inicializado
     }
   };
 
@@ -42,7 +49,7 @@ export const UrlShortenerProvider = ({ children }: { children: ReactNode }) => {
   const shortenUrl = async (url: string) => {
     setIsLoading(true);
     try {
-      const response = await axios.post("/shorten", { url });
+      const response = await axios.post(`${BACKEND_URL}/shorten`, { url });
       await fetchUrls(); // Actualizamos la lista
       return response.data.message;
     } catch (error) {
@@ -54,7 +61,7 @@ export const UrlShortenerProvider = ({ children }: { children: ReactNode }) => {
 
   const updateUrl = async (short_code: string, url: string) => {
     try {
-      await axios.put(`/shorten/${short_code}`, { url });
+      await axios.put(`${BACKEND_URL}/shorten/${short_code}`, { url });
       await fetchUrls();
       toast.success("URL updated successfully");
     } catch (error) {
@@ -65,7 +72,7 @@ export const UrlShortenerProvider = ({ children }: { children: ReactNode }) => {
 
   const deleteUrl = async (short_code: string) => {
     try {
-      await axios.delete(`/shorten/${short_code}`);
+      await axios.delete(`${BACKEND_URL}/shorten/${short_code}`);
       await fetchUrls();
       toast.success("URL deleted successfully");
     } catch (error) {
